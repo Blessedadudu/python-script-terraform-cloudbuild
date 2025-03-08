ff8e54fb245b95eefaba92ab22ab726c2a96f0a3
blessedadudu <adududuke@gmail.com>
2025-03-08 12:26:06 +0100

    update

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 import logging
 import time
-import os
-import threading
 import google.cloud.logging
-from dotenv import load_dotenv
-from welcome import print_welcome  # Import the welcome function
+import os
 
-# Load environment variables
-load_dotenv()
-
-SLEEP_TIME = int(os.getenv("SLEEP_TIME", 5))
-
-def main():
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-
-    if int(os.getenv("PRODUCTION", 0)) == 1:
+    if int(os.environ.get("PRODUCTION", 0)) == 1:
         logging_client = google.cloud.logging.Client()
         logging_client.setup_logging()
 
-    # Start welcome.py function in a separate thread
-    threading.Thread(target=print_welcome, daemon=True).start()
-
     while True:
-        logging.info("Sleeping for %d seconds", SLEEP_TIME)
-        time.sleep(SLEEP_TIME)
-
-if __name__ == "__main__":
-    main()
+        logging.info("Sleeping for 5 seconds")
+        time.sleep(5)
